090598f935bb03bbeba190a00254849f8bea98af
jeksterslab <learn.jeksterslab@gmail.com>
2023-03-13 16:59:19 +0000

    Automated build.

--- NEWS.md
+++
@@ -1,5 +0,0 @@
-# manBetaCIWald 0.9.1
-
-## Major
-
-* And so it begins.
--- R/manBetaCIWald-package.R
+++
@@ -1,7 +0,0 @@
-#' @aliases manBetaCIWald-package
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
